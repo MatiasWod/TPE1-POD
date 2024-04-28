03d20fd5e118881d1455ac18608505a7fd2d834d
Juan Segundo Arnaude <juansarnaude@gmail.com>
2024-04-28 20:51:14 -0300

    airlines locks

--- server/src/main/java/ar/edu/itba/pod/data/Exceptions/AirlineNotFoundException.java
+++ server/src/main/java/ar/edu/itba/pod/data/Exceptions/AirlineNotFoundException.java
@@ -1,4 +1,8 @@
 package ar.edu.itba.pod.data.Exceptions;
 
 public class AirlineNotFoundException extends RuntimeException {
+    private static final String MESSAGE = "Airline not found.";
+    public AirlineNotFoundException() {
+        super(MESSAGE);
+    }
 }
